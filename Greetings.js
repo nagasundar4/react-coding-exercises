4c0c4d2a5cf40d8810f3808d275babdd4f58622d
Nagarajan Sundaram <nagasundar@gmail.com>
2024-04-13 11:32:32 -0400

    Create Greetings.js

---
+++ Greetings.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function ({name}) {
+    return (
+        <div>
+            {/*Create a React component called Greetings that displays a greeting message like "Hello, World!".*/}
+    		<h1>Hello, World! {name}</h1>
+        </div>
+    );
+}
