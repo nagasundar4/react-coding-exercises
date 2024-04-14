ab358e0b74b933794dacf0d3917d784ebc6625f7
Nagarajan Sundaram <nagasundar@gmail.com>
2024-04-14 11:28:10 -0400

    Create SingleLineMultiLineCommentsExample.js

---
+++ SingleLineMultiLineCommentsExample.js
@@ -0,0 +1,10 @@
+<div>
+  {/* Single-line comments(In vanilla JavaScript, the single-line comments are represented by double slash(//)) */}
+  {`Welcome ${user}, let's play React`}
+</div>
+
+<div>
+  {/* Multi-line comments for more than
+   one line */}
+  {`Welcome ${user}, let's play React`}
+</div>
