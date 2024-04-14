51bb6525533f2cf48b4569a1cdff60c24dcd3129
Nagarajan Sundaram <nagasundar@gmail.com>
2024-04-14 11:24:16 -0400

    Update HigherOrderComponent.js

--- HigherOrderComponent.js
+++ HigherOrderComponent.js
@@ -1,1 +1,13 @@
+{/*
+A higher-order component (HOC) is a function that takes a component and returns a new component. Basically, it's a pattern that is derived from React's compositional nature.
+
+We call them pure components because they can accept any dynamically provided child component but they won't modify or copy any behavior from their input components.
+
+HOC can be used for many use cases:
+
+Code reuse, logic and bootstrap abstraction.
+Render hijacking.
+State abstraction and manipulation.
+Props manipulation.*/}
+
 const EnhancedComponent = higherOrderComponent(WrappedComponent);
