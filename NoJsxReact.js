91af51eda437e607fc375f3838e5ed2690aa4d96
Nagarajan Sundaram <nagasundar@gmail.com>
2024-04-14 10:53:34 -0400

    Create NoJsxReact.js

---
+++ NoJsxReact.js
@@ -0,0 +1,9 @@
+import { createElement } from 'react';
+
+export default function App() {
+  return createElement(
+    'h1',
+    { className: 'greeting' },
+    'Hello, this is a JSX Code!'
+  );
+}
